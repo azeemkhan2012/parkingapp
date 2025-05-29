2dbd4fd865c125ba0891d8b3597fcd262ff05aa0
Huzaifa sattar <huzaifasattar89@gmail.com>
2025-05-28 20:13:35 -0400

    created registration page with validation

--- App.tsx
+++ App.tsx
@@ -6,17 +6,19 @@
  */
 
 import React from 'react';
+import SignUpScreen from './src/components/signUpScreen';
 // import type {PropsWithChildren} from 'react';
-import LoginScreen from './src/components/loginScreen';
+// import LoginScreen from './src/components/loginScreen';
 
 // type SectionProps = PropsWithChildren<{
 //   title: string;
 // }>;
 function App(): React.JSX.Element {
- 
-
   return (
-    <LoginScreen />
+    <>
+      {/* // <LoginScreen /> */}
+      <SignUpScreen />
+    </>
   );
 }
 
--- src/style/style.js
+++ src/style/style.js
@@ -1,24 +1,23 @@
 import {StyleSheet} from 'react-native';
 
-
 export const styles = StyleSheet.create({
-container: {
+  container: {
     flex: 1,
-    backgroundColor:'#F4F6F6',
+    backgroundColor: '#F4F6F6',
     justifyContent: 'center',
     padding: 20,
   },
   title: {
     fontSize: 32,
     fontWeight: 'bold',
-    color:'#27AE60',
+    color: '#27AE60',
     alignSelf: 'center',
     marginBottom: 30,
   },
   input: {
     backgroundColor: '#fff',
     borderStyle: 'solid',
-    borderColor: "grey",
+    borderColor: 'grey',
     borderWidth: 1,
     padding: 15,
     borderRadius: 50,
@@ -42,9 +41,13 @@ container: {
     alignItems: 'center',
     justifyContent: 'center',
   },
-   logo: {
+  logo: {
     width: 150,
     height: 150,
-
+  },
+  error: {
+    color: 'blue',
+    marginBottom: 15,
+    marginLeft:10,
   },
 });
